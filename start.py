28148a3ff54d383a0ecda4f50ae49cc0f4eb3457
Awesome Guy <smath.study1@gmail.com>
2023-10-01 13:13:03 +0100

    added some git commands and explanation

--- start.py
+++ start.py
@@ -2,6 +2,12 @@ print("Hello world")
 print(100 + 1)
 
 # git checkout -b <branch name>
+# git remote add origin https://github.com/Aragorn44/delrep.git <- link to repository
+# git push -u origin master
+# git push --- because "-u" memorised what we used
+# git pull origin master 
+# we pull from origin that we set up using the link, and we pull the master branch from there
+
 
 print("This is a line on my first branch")
 
